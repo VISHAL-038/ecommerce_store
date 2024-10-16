026a1fa80abb4047a00f8681b9d6950025f7f1a2
vishal-038 <vk067414@gmail.com>
2024-10-16 22:01:23 +0530

    "commit"

---
+++ ecommerce_store/store/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+
+class Order(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
---
+++ ecommerce_store/store/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'store/product_detail.html', {'product': product})
